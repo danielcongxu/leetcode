b1caad824bf003670888bdb4b06ebfc2d36f2b9c
danielxc29 <danielxc29@163.com>
2017-07-10 16:04:17 +0800

    Optimize source code for Two Sum

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -12,26 +12,16 @@ class Solution
 vector<int> Solution::twoSum(vector<int>& nums, int target)
 {
 	map<int, int> mapValToIdx;
-	for (int i = 0; i < nums.size(); i++)
-	{
-		mapValToIdx.insert(pair<int, int>(nums[i], i));
-	}
 
 	for (int i = 0; i < nums.size(); i++)
 	{
 		map<int, int>::iterator mapIter = mapValToIdx.find(target - nums[i]);
 		if (mapIter != mapValToIdx.end() && i != mapIter->second)
 		{
-			cout << "Find matched pair" << endl;
-			cout << i << " " << mapIter->second << endl;
-			return vector<int>{i, mapIter->second};
+			return vector<int>{mapIter->second, i};
 		}
-	}
-	cout << "None of matched pairs" << endl;
-	return vector<int>();
-}
 
-void main()
-{
-	vector<int> temp = Solution::twoSum(vector<int>{2, 7, 11, 5}, 14);
+		mapValToIdx.insert(pair<int, int>(nums[i], i));
+	}
+	return vector<int>();
 }
